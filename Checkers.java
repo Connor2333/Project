ae2dc0935fa65e8cf63c6052b29a1abfe32ef5b9
Connor2333 <47461196+Connor2333@users.noreply.github.com>
2019-02-28 17:26:45 -0700

    Update Checkers.java

--- Checkers.java
+++ Checkers.java
@@ -1,5 +1,18 @@
+/* java code of checkers
+
+*/
 public class Checkers{
-	
+	public static void main(String args[]){
+		new Myframe();
+	}
+//load frame 
+class MyFrame {
+}
+/*class for checkers. single checker can move, jump-over and upgrade to advanced stage.
+2 types in total(red and black)
+*/
+class singleChecker{
+}
 
 
 }
