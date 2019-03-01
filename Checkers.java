172e9dc487cfe3d00778a632371e378cef73342d
Connor2333 <47461196+Connor2333@users.noreply.github.com>
2019-02-28 18:21:03 -0700

    Update Checkers.java

--- Checkers.java
+++ Checkers.java
@@ -5,14 +5,29 @@ public class Checkers{
 	public static void main(String args[]){
 		new Myframe();
 	}
-//load frame 
+//load frame, include the chessboard. using 2D list for represent the position of checkers.
 class MyFrame {
 }
 /*class for checkers. single checker can move, jump-over and upgrade to advanced stage.
-2 types in total(red and black)
+2 types in total(red and black),black regard as going up and red as going down
+
 */
 class singleChecker{
 }
-
+//baisc stage: moving to the front 1 step and go to left or right
+//advanced stage: move to front or reverse 1 step and go left and right
+//move: 2D list coordinate system
+class move{
+}
+//valid move or not(move=false)
+//win or lose (# of red or black =0)
+	
+class gameConfig{
+}
+//most confusing part. what kind of algorithm?
+class AI{
+}
+class humanPlayer{
+}
 
 }
