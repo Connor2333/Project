69195375222ed218e0999756ad8fffc9aaf6b03e
Huilin11037 <48135853+Huilin11037@users.noreply.github.com>
2019-04-12 08:36:42 -0600

    Add files via upload

---
+++ CheckersTest.java
@@ -0,0 +1,107 @@
+import java.io.*;
+import java.util.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import junit.framework.TestCase;
+
+
+
+public class CheckersTest {
+
+    @Test
+    public void testBoard_1(){
+
+
+        Checkers game = new Checkers();
+        String s = "  1 2 3 4 5 6 7 8 x\n" +
+                "1 r _ r _ r _ r _ \n" +
+                "2 _ r _ r _ r _ r \n" +
+                "3 r _ r _ r _ r _ \n" +
+                "4 _ _ _ _ _ _ _ _ \n" +
+                "5 _ _ _ _ _ _ _ _ \n" +
+                "6 _ b _ b _ b _ b \n" +
+                "7 b _ b _ b _ b _ \n" +
+                "8 _ b _ b _ b _ b \n" +
+                "y\n" ;
+        
+
+    }
+
+
+    @Test
+    public void testBoard_2(){
+        Checkers game = new Checkers();
+
+    }
+
+    @Test
+    public void testBoard_3(){
+
+    }
+
+    @Test
+    public void testBoard_4(){
+
+    }
+
+    @Test
+    public void testBoard_5(){
+
+    }
+
+    @Test
+    public void testMovement_1(){
+
+    }
+
+    @Test
+    public void testMovement_2(){
+
+    }
+
+    @Test
+    public void testMovement_3(){
+
+    }
+
+    @Test
+    public void testMovement_4(){
+
+    }
+
+    @Test
+    public void testMovement_5(){
+
+    }
+
+    @Test
+    public void testEat_1(){
+
+    }
+
+    @Test
+    public void testEat_2(){
+
+    }
+
+    @Test
+    public void testEat_3(){
+
+    }
+
+    @Test
+    public void testEat_4(){
+
+    }
+
+    @Test
+    public void testEat_5(){
+
+    }
+
+
+
+}
