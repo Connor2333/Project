c04ca715d93b9d34d84d8fad70df57e64cde69b3
Connor2333 <47461196+Connor2333@users.noreply.github.com>
2019-04-12 19:07:20 -0600

    Update MoveType.java

--- checkers/MoveType.java
+++ checkers/MoveType.java
@@ -2,6 +2,7 @@ package checkers;
 
 /**
  * Enumeration of checkers moving type
+ *Citation: https://github.com/AlmasB/FXTutorials/tree/master/src/com/almasb/checkers
  * @author Group 29
  */
 public enum MoveType {
